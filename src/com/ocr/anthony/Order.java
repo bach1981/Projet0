6668cfe93a6a4acf01efaab3ba298341005d2e3e
bach <bach.81@hotmail.fr>
2018-07-19 12:26:32 +0200

    rajouter le menu poser la question pour le choix du menu afficher le choix

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -5,6 +5,11 @@ public class Order {
      * Display all available menus in the restaurant.
      */
     public void displayAvailableMenu() {
+        System.out.println("Choix menu");
+        System.out.println("1 - poulet");
+        System.out.println("2 - boeuf");
+        System.out.println("3 - végétarien");
+        System.out.println("Que souhaitez-vous comme menu ?");
 
     }
     /**
@@ -12,6 +17,8 @@ public class Order {
      * @param nbMenu The selected menu.
      */
     public void displaySelectedMenu(int nbMenu) {
+
+        System.out.println("Vous avez choisi le menu " + nbMenu);
 
     }
 }
